24f610f5e30a054842dd11af03fa550ef7822941
spattan <pattanshakeel@gmail.com>
2026-05-19 13:07:45 +0530

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// WHATSAPP SHARE
+
 document
 .getElementById('shareBtn')
 .addEventListener('click',()=>{
@@ -10,3 +12,38 @@ window.open(
 );
 
 });
+
+// COUNTDOWN TIMER
+
+const target =
+new Date("June 7, 2026 18:00:00").getTime();
+
+const countdown =
+document.getElementById('countdown');
+
+setInterval(()=>{
+
+const now = new Date().getTime();
+
+const diff = target - now;
+
+if(diff <= 0){
+
+countdown.innerHTML = "The Celebration Has Started";
+
+return;
+}
+
+const days =
+Math.floor(diff / (1000 * 60 * 60 * 24));
+
+const hours =
+Math.floor(
+(diff % (1000 * 60 * 60 * 24)) /
+(1000 * 60 * 60)
+);
+
+countdown.innerHTML =
+`${days} Days • ${hours} Hours Remaining`;
+
+},1000);
